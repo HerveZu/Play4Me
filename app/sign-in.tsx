24a44a8e9be3937cf1c3fd4a67768c379093db71
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2025-11-22 21:48:43 +0100

    Add manual redirection for native after social sign-in completion

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,13 +1,24 @@
 import { authClient } from '@/providers/auth'
 import { Button, Host, Text, VStack } from '@expo/ui/swift-ui'
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
 
 export default function SocialSignIn() {
+  const router = useRouter()
+  const { data } = authClient.useSession()
+
   const handleLogin = async () => {
     await authClient.signIn.social({
       provider: 'spotify',
       callbackURL: '/',
     })
   }
+
+  // the redirection occurs on a web popup
+  // thus we need to redirect manually on native
+  useEffect(() => {
+    data && router.push('/')
+  }, [data, router])
 
   return (
     <Host style={{ flex: 1 }}>
