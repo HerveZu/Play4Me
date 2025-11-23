2331bbb144f80129abab9e9c3d70655d6145952b
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2025-11-23 12:02:56 +0100

    Refactor playlist UI: extract `PlaylistItem` and `PlaylistPreview` components, implement long-press context menu, and enhance playlist preview with playback details.

--- app/(authenticated)/playlists/index.tsx
+++ app/(authenticated)/playlists/index.tsx
@@ -17,6 +17,10 @@ import { UserPlaylist } from '@/app/(authenticated)/api/playlist/index+api'
 import { formatRelative, startOfDay } from 'date-fns'
 import { capitalizeFirstLetter } from '@/lib/utils'
 import { PlayingIndicator } from '@/lib/PlayingIndicator'
+import { frame, padding } from '@expo/ui/swift-ui/modifiers'
+import { useQuery } from '@tanstack/react-query'
+import { getClientSpotifyApi } from '@/lib/spotify/client'
+import { useWindowDimensions } from 'react-native'
 
 export default function HomePage() {
   const { playlists, deletePlaylist } = usePlaylists()
@@ -67,52 +71,100 @@ export default function HomePage() {
             }
           >
             {playlists.map((playlist) => (
-              <HStack
-                key={playlist.id}
-                onPress={() => router.push(`/player?playlistId=${playlist.id}`)}
-              >
-                <VStack alignment={'leading'} spacing={10}>
-                  <HStack spacing={10}>
-                    {playlist.active ? (
-                      <PlayingIndicator />
-                    ) : (
-                      <Image systemName={'music.note'} size={18} />
-                    )}
-                    <Text weight={'semibold'}>{playlist.title}</Text>
+              <ContextMenu activationMethod={'longPress'} key={playlist.id}>
+                <ContextMenu.Preview>
+                  <PlaylistPreview playlist={playlist} />
+                </ContextMenu.Preview>
+                <ContextMenu.Trigger>
+                  <HStack
+                    onPress={() =>
+                      router.push(`/player?playlistId=${playlist.id}`)
+                    }
+                    alignment={'top'}
+                  >
+                    <PlaylistItem playlist={playlist} />
+                    <Spacer />
                   </HStack>
-                  <Text weight={'light'}>{playlist.description}</Text>
-                </VStack>
-                <Spacer />
-
-                <ContextMenu>
-                  <ContextMenu.Trigger>
-                    <Button systemImage={'chevron.right'} variant={'glass'} />
-                  </ContextMenu.Trigger>
-                  <ContextMenu.Items>
-                    <Button
-                      systemImage={'play'}
-                      onPress={() =>
-                        router.push(`/player?playlistId=${playlist.id}`)
-                      }
-                    >
-                      Play
-                    </Button>
-                    <Button
-                      role={'destructive'}
-                      systemImage={'trash'}
-                      onPress={() =>
-                        deletePlaylist({ playlistId: playlist.id })
-                      }
-                    >
-                      Delete
-                    </Button>
-                  </ContextMenu.Items>
-                </ContextMenu>
-              </HStack>
+                </ContextMenu.Trigger>
+                <ContextMenu.Items>
+                  <Button
+                    systemImage={'play'}
+                    onPress={() =>
+                      router.push(`/player?playlistId=${playlist.id}`)
+                    }
+                  >
+                    Play
+                  </Button>
+                  <Button
+                    role={'destructive'}
+                    systemImage={'trash'}
+                    onPress={() => deletePlaylist({ playlistId: playlist.id })}
+                  >
+                    Delete
+                  </Button>
+                </ContextMenu.Items>
+              </ContextMenu>
             ))}
           </Section>
         ))}
       </Form>
     </Host>
+  )
+}
+
+function PlaylistItem({ playlist }: { playlist: UserPlaylist }) {
+  return (
+    <VStack alignment={'leading'} spacing={10}>
+      <HStack spacing={10}>
+        {playlist.active ? (
+          <PlayingIndicator />
+        ) : (
+          <Image systemName={'music.note'} size={18} />
+        )}
+        <Text weight={'semibold'}>{playlist.title}</Text>
+      </HStack>
+      <Text weight={'light'} lineLimit={1}>
+        {playlist.description}
+      </Text>
+    </VStack>
+  )
+}
+
+function PlaylistPreview({ playlist }: { playlist: UserPlaylist }) {
+  const { data } = useQuery({
+    queryKey: ['playlist-playback', playlist.active],
+    queryFn: async () => {
+      if (!playlist.active) return null
+      const spotifyApi = await getClientSpotifyApi()
+      return await spotifyApi.player.getPlaybackState()
+    },
+  })
+  const { width } = useWindowDimensions()
+
+  return (
+    <VStack
+      modifiers={[frame({ width: width }), padding({ vertical: 20 })]}
+      spacing={40}
+    >
+      <VStack
+        alignment={'leading'}
+        spacing={10}
+        modifiers={[padding({ horizontal: 20 })]}
+      >
+        <Text weight={'semibold'} size={20}>
+          {playlist.title}
+        </Text>
+        <Text>{playlist.description}</Text>
+      </VStack>
+
+      {data?.device && (
+        <HStack spacing={10}>
+          <Spacer />
+          <PlayingIndicator />
+          <Text>{`Playing on ${data?.device.name}`}</Text>
+          <Spacer />
+        </HStack>
+      )}
+    </VStack>
   )
 }
